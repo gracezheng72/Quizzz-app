2d321eedb69a7e94f22c3062ad32a5b1f106aa77
gracezheng72 <gracezheng72@gmail.com>
2025-04-07 15:32:04 -0700

    Update app.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders welcome text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const welcomeText = screen.getByText(/welcome to the quizzz app!/i);  // Adjusting to match the actual text
+  expect(welcomeText).toBeInTheDocument();
 });
